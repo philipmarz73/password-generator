8de441d2babe923d09fe16ae04efcc485ee617d4
Phlipper <marzullop73@gmail.com>
2021-02-07 18:26:36 -0500

    added array vars w/numbers, symbols, letters

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
+// defining arrays of numbers, symbols and letters for password
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var upCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var symbols = ['!','@','#','$','%','^','&','*','(',')','-','_','+','=','/','{','}','[',']','|'];
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
